4072f0df36613c06ff07cc7dec4de18b0bde2160
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-06 13:45:13 -0500

    Updated from Brython Server: 12/6/2017 1:45:12 PM

--- fileDemo.py
+++ fileDemo.py
@@ -7,7 +7,7 @@ dictionary = open('engmix.txt')
 #finding number of words
 wordCount = 0 
 for word in dictionary:
-    if 'greg' in word:
-        print(word)
+    if 'greg' in word: #looks for words with greg in it
+        print(word.strip()) #stops it from going to the next line when printed
     wordCount += 1
 print('There are', wordCount, 'words in the dictionary.')
