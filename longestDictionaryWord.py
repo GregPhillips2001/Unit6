29398dc9daf4beeffa6fc2fac57ad50f42ebc96b
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-06 13:53:39 -0500

    Updated from Brython Server: 12/6/2017 1:53:39 PM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,11 @@
+#Greg Phillips
+#12/6/17
+#longestDictionaryWord.py 
 
+dictionary = open('engmix.txt')
+
+word = 1
+for words in dictionary:
+    if word < words:
+        word = words
+print(word)
