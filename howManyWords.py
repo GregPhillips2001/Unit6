29dba9ea728a4e1be8c93f49f41c7eca3d4eaf9c
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-07 14:43:07 -0500

    Updated from Brython Server: 12/7/2017 2:43:06 PM

--- howManyWords.py
+++ howManyWords.py
@@ -9,11 +9,11 @@ words = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 for w in dictionary:
     length = len(w)
-    for i in range(1,30)
-    if length == i: 
-        words[i-1] += 1
+    for i in range(1,30):
+        if length == i: 
+            words[i-1] += 1
             
-
-print('There are', words[0], 'one letter words in the dictionary.')
+for i in range(1,25):
+    print('There are', words[i], i, 'letter words in the dictionary.')
 
 
