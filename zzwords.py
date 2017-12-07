7d95f88194566304065af9db8a91ab095d3e5096
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-07 14:12:12 -0500

    Updated from Brython Server: 12/7/2017 2:12:10 PM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,9 @@
+#Greg Phillips
+#12/7/17
+#zzword.py
 
+dictionary = open('engmix.txt')
+
+for word in dictionary:
+    if 'zz' in word: 
+        print(word.strip()) #stops it from going to the next line when printed
