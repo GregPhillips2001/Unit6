36aad924e58170fa533aa6c55a1b0fff6650c2dd
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-07 14:24:49 -0500

    Updated from Brython Server: 12/7/2017 2:24:48 PM

--- howManyWords.py
+++ howManyWords.py
@@ -8,9 +8,9 @@ dictionary = open('engmix.txt')
 wordCount = 0 
 
 for word in dictionary:
-    if len(word) == 1:
+    if len(word) == 2:
         wordCount += 1
         
-print('There are', wordCount, 'one letter words in the dictionary.')
+print('There are', wordCount, 'two letter words in the dictionary.')
 
 
