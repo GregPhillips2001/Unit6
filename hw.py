71c04a28fb629c817b41e1752bea11800530bdc2
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-08 14:41:02 -0500

    Updated from Brython Server: 12/8/2017 2:41:01 PM

--- hw.py
+++ hw.py
@@ -2,17 +2,20 @@
 #12/7/17
 #hw.py 
 
-#
+#is word in dictionary
 dictionary = open('engmix.txt')
 
 word = input('enter a word, ')
 
 yes = False
 for w in dictionary:
-    if word == w: 
+    if w.strip()==word: 
         yes = True
         break
+
 if yes == True:
-    print(word, 'is not in the dictionary')
-else:
     print(word, 'is in the dictionary')
+else:
+    print(word, 'is not in the dictionary')
+
+#dictionary list
