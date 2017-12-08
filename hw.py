ade83f5026a90c2c45ee59827a57aaf6c80f2dd6
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-08 14:30:01 -0500

    Updated from Brython Server: 12/8/2017 2:30:00 PM

--- hw.py
+++ hw.py
@@ -7,5 +7,6 @@ dictionary = open('engmix.txt')
 word = input('enter a word, ')
 
 for w in dictionary:
-    if word == w: 
-        print(word, 'is in the dictionary')
+    if word in w: 
+        break
+print(word, 'is in the dictionary')
