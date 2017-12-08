12757baeaf51fabb9a774a9e1fbe4336dbc627d6
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-08 14:52:17 -0500

    Updated from Brython Server: 12/8/2017 2:52:17 PM

--- hw.py
+++ hw.py
@@ -16,7 +16,7 @@ for w in dictionary:
 if yes == True:
     print(word, 'is in the dictionary')
 else:
-    print(word, 'is not in the dictionary')"""
+    print(word, 'is not in the dictionary')
 
 #dictionary list
 dictionary = open('engmix.txt')
@@ -25,4 +25,11 @@ list = []
 for w in dictionary:
     list.append(w.strip())
 num = int(input('enter a number, '))
-print(list[num-1])
+print(list[num-1])"""
+
+#exclamation point
+
+file = open('howManyWords.py')
+
+for line in file:
+    print(line.strip(), '!')
