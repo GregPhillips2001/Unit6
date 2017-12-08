d44f019f081679bc69bfb709bfdcd01f7fb6f34e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-08 14:45:45 -0500

    Updated from Brython Server: 12/8/2017 2:45:44 PM

--- hw.py
+++ hw.py
@@ -2,7 +2,7 @@
 #12/7/17
 #hw.py 
 
-#is word in dictionary
+"""#is word in dictionary
 dictionary = open('engmix.txt')
 
 word = input('enter a word, ')
@@ -16,13 +16,13 @@ for w in dictionary:
 if yes == True:
     print(word, 'is in the dictionary')
 else:
-    print(word, 'is not in the dictionary')
+    print(word, 'is not in the dictionary')"""
 
 #dictionary list
 dictionary = open('engmix.txt')
 list = []
 
 for w in dictionary:
-    list.append[w.strip()]
+    list.append(w.strip())
 num = input('enter a number, ')
 print(list[num-1])
