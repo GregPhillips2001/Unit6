7dcdf570ff860e9607081b58f6807ece3ebafad0
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-11 14:24:01 -0500

    Updated from Brython Server: 12/11/2017 2:24:00 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,10 @@
+#Greg Phillips
+#12/11/17
+#warmup16.py - how to read a file
 
+dictionary = open('engmix.txt')
+
+for w in dictionary:
+    if w[0] == 'g':
+        if w[-1] == 'p':
+            print(w)
