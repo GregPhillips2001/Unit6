591aa3caf4d75b5007a9d2dfc60d17f7a67de525
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-13 13:30:32 -0500

    Updated from Brython Server: 12/13/2017 1:30:30 PM

--- quizz.py
+++ quizz.py
@@ -2,11 +2,11 @@
 #12/13/17
 #quizz.py 
 
-dictionary = open('engmix.txt')
+"""dictionary = open('engmix.txt')
 
 wordcount = 0
 for w in dictionary:
     if w.strip() != '':
         if w.strip()[0] == 'r':
             wordcount += 1
-print('There are', wordcount, 'words in the dictionary that start with r.')
+print('There are', wordcount, 'words in the dictionary that start with r.')"""
