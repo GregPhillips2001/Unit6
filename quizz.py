4702a8eb05f61adce58299613fb1bddce6da1bf2
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-12-13 13:26:38 -0500

    Updated from Brython Server: 12/13/2017 1:26:37 PM

--- quizz.py
+++ quizz.py
@@ -1,1 +1,11 @@
+#Greg Phillips
+#12/13/17
+#quizz.py 
 
+dictionary = open('engmix.txt')
+
+for w in dictionary:
+    if w.strip() != '':
+        if w.strip()[0] == 'r':
+            print(w)
+        
